b892dcb12106174da76c2a6e1b93a44706933576
Colleen Bertoni <bertoni@anl.gov>
2024-07-08 13:27:41 -0500

    Update gpu_tile_compact.sh

--- microbenchmark/gpu_tile_compact.sh
+++ microbenchmark/gpu_tile_compact.sh
@@ -5,6 +5,18 @@ num_tile=2
 num_socket=2
 
 _MPI_RANKID=$PALS_LOCAL_RANKID
+
+# Get the RankID from different launcher                                                                                                                                                                                    
+if [[ -v MPI_LOCALRANKID ]]; then
+  _MPI_RANKID=$MPI_LOCALRANKID
+elif [[ -v PALS_LOCAL_RANKID ]]; then
+  _MPI_RANKID=$PALS_LOCAL_RANKID
+elif [[ -v OMPI_COMM_WORLD_LOCAL_RANK ]]; then
+  _MPI_RANKID=$OMPI_COMM_WORLD_LOCAL_RANK
+else
+  echo "Unknown MPI, adjust this script."
+fi
+
 gpu_id=$((_MPI_RANKID / num_tile))
 tile_id=$((_MPI_RANKID % num_tile))
 
