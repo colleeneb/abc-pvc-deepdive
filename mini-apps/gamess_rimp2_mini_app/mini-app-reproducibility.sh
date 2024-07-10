0c79989b06d53e733baed1674da0d5fd3d919c83
JaeHyuk Kwack <jkwack@anl.gov>
2024-07-10 12:57:56 -0500

    [GAMESS RIMP2] Update reproducibility script for JLSE H100

--- mini-apps/gamess_rimp2_mini_app/mini-app-reproducibility.sh
+++ mini-apps/gamess_rimp2_mini_app/mini-app-reproducibility.sh
@@ -1,9 +1,11 @@
 # Downloading source code
 $ git clone -b SC24_PMBS --single-branch https://github.com/jkwack/GAMESS_RI-MP2_MiniApp.git
-
-# Setting the environment on Aurora or Sunspot
 $ cd GAMESS_RI-MP2_MiniApp
-$ source source_me_Sunspot 
+
+
+# Setting the environment
+$ source source_me_Sunspot      # on Aurora or Sunspot
+$ source source_me_JLSE_H100    # on JLSE (H100 nodes)
  
 # Compiling the code
 $ make
@@ -15,8 +17,18 @@ $ OMP_NUM_THREADS=1 mpirun -n 1 gpu_tile_compact.sh ./rimp2-mkl-offload w60.rand
 ## On a single GPU (i.e., 2 stacks) on Aurora or Sunspot
 $ OMP_NUM_THREADS=1 mpirun -n 2 gpu_tile_compact.sh ./rimp2-mkl-offload w60.rand
 
+## On a half node (i.e., 3 GPUs) on Aurora or Sunspot
+$ OMP_NUM_THREADS=1 mpirun -n 6 gpu_tile_compact.sh ./rimp2-mkl-offload w60.rand
+
 ## On a single node (i.e., 6 GPUs) on Aurora or Sunspot
 $ OMP_NUM_THREADS=1 mpirun -n 12 gpu_tile_compact.sh ./rimp2-mkl-offload w60.rand
+
+## On a single H100 on JLSE
+$ OMP_NUM_THREADS=1 mpirun -n 1 ./set_affinity_gpu.sh ./rimp2-cublas w60.rand
+
+## On a single node (i.e., 4 H100 GPUs) on JLSE
+$ OMP_NUM_THREADS=1 mpirun -n 4 ./set_affinity_gpu.sh ./rimp2-cublas w60.rand
+
 
 
 # Extracting the FOM value
