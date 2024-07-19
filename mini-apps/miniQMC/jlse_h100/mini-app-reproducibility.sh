c28ff2b0065ecd77313c83aef2f969f5b9a4b901
Ye Luo <yeluo@anl.gov>
2024-07-19 00:06:46 -0500

    Add miniqmc/aurora_pvc

--- mini-apps/miniQMC/jlse_h100/mini-app-reproducibility.sh
+++ mini-apps/miniQMC/jlse_h100/mini-app-reproducibility.sh
@@ -6,7 +6,7 @@ fi
 module use /soft/modulefiles
 module use /soft/packaging/spack-builds/modules/linux-opensuse_leap15-x86_64
 module load cmake/3.28.3 public_mkl/2019 llvm/release-18.1.0 cuda/12.3.0 openmpi/4.1.1-llvm
-module list > module.list
+module list >& module.list
 # Compiling the code
 build_folder=build_h100_clang18_offload_real_MP
 cmake -B $build_folder -DCMAKE_CXX_COMPILER=mpicxx -DQMC_MPI=ON -DQMC_ENABLE_CUDA=ON -DENABLE_OFFLOAD=ON -DQMC_GPU_ARCHS=sm_90 -DQMC_MIXED_PRECISION=ON ../miniqmc
@@ -22,4 +22,4 @@ do
   mpirun -n 4 --map-by ppr:4:node:PE=24 ../../gpu_tile_compact.sh $build_folder/bin/miniqmc_sync_move -P -g "2 2 1" -w 320 -n 1 > h100_r4_t8.s$series.out
 done
 # Extracting the FOM value
-grep "Diffusion throughput" h100_r?_t8.s?.out
+grep "Diffusion throughput" *_r*_t8.s?.out
